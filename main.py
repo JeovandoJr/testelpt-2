1c93b31c8e1e8393dbdb4f871d1ad15567ff02b9
Jeovando Júnior <139470590+JeovandoJr@users.noreply.github.com>
2023-07-27 18:41:35 +0000

    Foi corrigido a entrada de dados para float.

--- main.py
+++ main.py
@@ -23,30 +23,30 @@ def menu ():
 while True:
     menu()
     print("Digite os valores:")
-    valor1 = input("Digite o valor 1: ")
-    valor2 = input("Digite o valor 2: ")
+    valor1 = float(input("Digite o valor 1: "))
+    valor2 = float(input("Digite o valor 2: "))
 
     opcao = input("Qual a opção de operação? ")
 
     if opcao == "1":
         resultado = soma(valor1, valor2)
-        print(f"Resultado da Soma: {resultado}")
+        print(f"Resultado da Soma: {resultado}\n")
 
     elif opcao == "2":
         resultado = subtrai(valor1, valor2)
-        print(f"Resultado da Subtração: {resultado}")
+        print(f"Resultado da Subtração: {resultado}\n")
     
     elif opcao == "3":
         resultado = multiplica(valor1, valor2)
-        print(f"Resultado da Multiplicação: {resultado}")
+        print(f"Resultado da Multiplicação: {resultado}\n")
 
     elif opcao == "4":
         resultado = divide(valor1, valor2)
-        print(f"Resultado da Divisão: {resultado}")
+        print(f"Resultado da Divisão: {resultado}\n")
     
     elif opcao == "5":
         break
 
     else:
-        print("Opção Inválida.")
+        print("Opção Inválida.\n")
     
