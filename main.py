6c241c23d71440ac6bc59ef914b009eb89762fd3
Jeovando Júnior <139470590+JeovandoJr@users.noreply.github.com>
2023-07-27 18:35:19 +0000

    Implementei a Features Vásicas da Mini-Calculadora.

--- main.py
+++ main.py
@@ -2,4 +2,51 @@ def soma (valor1, valor2):
     return valor1 + valor2
 
 def subtrai (valor1, valor2):
-    return(valor1 - valor2)
+    return valor1 - valor2
+
+def multiplica (valor1 , valor2):
+    return valor1 * valor2
+
+def divide (valor1, valor2):
+    if valor2 == 0:
+        print("Não é possível dividir.")
+    return valor1 / valor2
+
+def menu ():
+    print("1. Soma")
+    print("2. Subtrai")
+    print("3. Multiplica")
+    print("4. Divide")
+    print("5. Sair")
+
+
+while True:
+    menu()
+    print("Digite os valores:")
+    valor1 = input("Digite o valor 1: ")
+    valor2 = input("Digite o valor 2: ")
+
+    opcao = input("Qual a opção de operação? ")
+
+    if opcao == "1":
+        resultado = soma(valor1, valor2)
+        print(f"Resultado da Soma: {resultado}")
+
+    elif opcao == "2":
+        resultado = subtrai(valor1, valor2)
+        print(f"Resultado da Subtração: {resultado}")
+    
+    elif opcao == "3":
+        resultado = multiplica(valor1, valor2)
+        print(f"Resultado da Multiplicação: {resultado}")
+
+    elif opcao == "4":
+        resultado = divide(valor1, valor2)
+        print(f"Resultado da Divisão: {resultado}")
+    
+    elif opcao == "5":
+        break
+
+    else:
+        print("Opção Inválida.")
+    
